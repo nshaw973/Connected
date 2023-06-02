3588e06dc68ec7edef54f696b1f0ac9d6a2e417a
Shawn Lapuz <lapuzshawn@gmail.com>
2023-06-01 21:20:22 -0700

    git

--- client/src/App.js
+++ client/src/App.js
@@ -8,15 +8,33 @@ import './index.css'
 
 
 const App = () => {
+
+  const [currentPage, setCurrentPage] = useState('MainPage');
+  const renderPage = () => {
+    switch (currentPage) {
+      case 'MainPage':
+        return <MainPage />;
+      case 'Login':
+        return <Login />;
+      case 'Signup':
+        return <Signup />
+      default:
+        return <h1> 404 Page Not Found</h1>
+    }
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
   return (
     <>
-      {/* <MainPage /> */}
+      {/* <Navbar currentPage={currentPage} handlePageChange={handlePageChange} />
+      {renderPage()}
+      <Footer /> */}
       {/* <AboutPage /> */}
       {/* <HomePage /> */}
       <CandidatePage />
     </>
-
-  )
+  );
 };
 
 export default App;
