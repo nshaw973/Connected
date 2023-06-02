c4362bfdde3cfe078fe76dbfe5766b948e366c73
nshaw973 <nshaw973@gmail.com>
2023-06-01 20:10:04 -0700

    added a couple things like the navbar, and footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,53 @@
 import React from "react";
-import MainPage from "./components/MainPage";
+
+import MainPage from "./pages/MainPage";
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 import './index.css'
+
+
+
+// Once apollo is up and running, unComment this
+/* 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'; */
+
+/* 
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+
+}); 
+*/
+
+/* const App = () => {
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <div className="flex-column justify-center align-center min-100-vh bg-primary">
+          <Routes>
+            <Route 
+              path="/" 
+              element={<MainPage />}
+            />
+          </Routes>
+        </div>
+      </Router>
+    </ApolloProvider>
+  );
+}; */
+
+// Delete this once Apollo is running
+
 
 const App = () => {
   return (
     <>
-      <MainPage />
+    <Navbar />
+    <MainPage />
+    <Footer />
     </>
-
   )
-};
+}
 
 export default App;
