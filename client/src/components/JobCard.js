6964c9c2e02f7a189a889ae24eecaa5bb51b34e3
Shawn Lapuz <lapuzshawn@gmail.com>
2023-06-06 18:40:33 -0700

    typedefs placed

---
+++ client/src/components/JobCard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+
+function JobCard({ title }) {
+  return (
+    <div className="job-card">
+      
+    </div>
+  );
+}
+
+export default JobCard;
+
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,16 @@
 const { gql } = require('apollo-server-express');
 
-const typeDefs = gql``;
+const typeDefs = gql`
+   type Query {
+    me: User
+   }
+
+    type User {
+    username: String!
+    email: String!
+    password: String!
+
+    }
+`;
 
 module.exports = typeDefs;
