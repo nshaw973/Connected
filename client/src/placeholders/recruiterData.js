d913ce703e308d38d44a16f9c963d5d2164df512
Shawn Lapuz <lapuzshawn@gmail.com>
2023-06-06 21:10:55 -0700

    recruiter placeholders

---
+++ client/src/placeholders/recruiterData.js
@@ -0,0 +1,137 @@
+const recruiterData = [
+    {
+      name: 'John Doe',
+      jobTitle: 'Front End Developer Recruiter',
+      agency: 'Robert Half',
+      contactEmail: 'john.doe@roberthalf.com',
+      jobPostings: [
+        {
+          title: 'Front End Dev Opportunity',
+          deployedLink: '/',
+          description: 'A Front-End Developer is responsible for developing new user-facing features, determining the structure and design of web pages, building reusable codes, optimizing page loading times, and using a variety of markup languages to create the web pages.',
+        },
+        // Other job postings by John Doe from Robert Half
+      ],
+    },
+    {
+      name: 'Jane Doe',
+      jobTitle: 'Back End Developer Recruiter',
+      agency: 'Creative Staffing',
+      contactEmail: 'jane.doe@creativestaffing.com',
+      jobPostings: [
+        {
+          title: 'Back End Dev Opportunity',
+          deployedLink: '/',
+          description: 'Backend developer responsibilities include creating, maintaining, testing, and debugging the entire back end of an application or system. This includes the core application logic, databases, data and application integration, API, and other processes taking place behind the scenes.',
+        },
+        // Other job postings by Jane Doe from Creative Staffing
+      ],
+    },
+    {
+      name: 'Michael Smith',
+      jobTitle: 'Full Stack Developer Recruiter',
+      agency: 'Tech Solutions Inc.',
+      contactEmail: 'michael.smith@techsolutions.com',
+      jobPostings: [
+        {
+          title: 'Full Stack Developer',
+          deployedLink: '/',
+          description: 'A Full Stack Developer is responsible for developing and maintaining both the front-end and back-end portions of a web application. They should be proficient in both front-end languages like HTML, CSS, JavaScript, as well as back-end languages like Node.js, Python, or Ruby on Rails.',
+        },
+        // Other job postings by Michael Smith from Tech Solutions Inc.
+      ],
+    },
+    {
+      name: 'Emily Johnson',
+      jobTitle: 'UI/UX Designer Recruiter',
+      agency: 'DesignWorks',
+      contactEmail: 'emily.johnson@designworks.com',
+      jobPostings: [
+        {
+          title: 'UI/UX Designer',
+          deployedLink: '/',
+          description: 'A UI/UX Designer is responsible for creating visually appealing and user-friendly interfaces for digital products. They should have a good understanding of user experience principles, wireframing, prototyping, and using design tools like Sketch, Adobe XD, or Figma.',
+        },
+        // Other job postings by Emily Johnson from DesignWorks
+      ],
+    },
+    {
+      name: 'David Anderson',
+      jobTitle: 'Data Scientist Recruiter',
+      agency: 'Data Analytics Corp',
+      contactEmail: 'david.anderson@dataanalyticscorp.com',
+      jobPostings: [
+        {
+          title: 'Data Scientist',
+          deployedLink: '/',
+          description: 'A Data Scientist is responsible for analyzing large and complex datasets to discover patterns, extract insights, and make data-driven decisions. They should have a strong background in statistics, programming languages like Python or R, and machine learning algorithms.',
+        },
+        {
+          title: 'Data Analyst',
+          deployedLink: '/',
+          description: 'A Data Analyst is responsible for analyzing large and complex datasets to discover patterns, extract insights, and make data-driven decisions. They should have a strong background in statistics, programming languages like Python or R, and machine learning algorithms.',
+        },
+        // Other job postings by David Anderson from Data Analytics Corp
+      ],
+    },
+    {
+      name: 'Jennifer Smith',
+      jobTitle: 'Software Engineer Recruiter',
+      agency: 'Tech Innovators',
+      contactEmail: 'jennifer.smith@techinnovators.com',
+      jobPostings: [
+        {
+          title: 'Software Engineer',
+          deployedLink: '/',
+          description: 'A Software Engineer is responsible for designing, developing, and maintaining software applications. They should have strong programming skills in languages like Java, C++, or C#, and be familiar with software development methodologies and tools.',
+        },
+        // Other job postings by Jennifer Smith from Tech Innovators
+      ],
+    },
+    {
+      name: 'Mark Thompson',
+      jobTitle: 'Network Administrator Recruiter',
+      agency: 'IT Solutions Ltd.',
+      contactEmail: 'mark.thompson@itsolutionsltd.com',
+      jobPostings: [
+        {
+          title: 'Network Administrator',
+          deployedLink: '/',
+          description: 'A Network Administrator is responsible for managing and maintaining computer networks within an organization. They should have knowledge of network protocols, troubleshooting skills, and experience with network monitoring tools.',
+        },
+        // Other job postings by Mark Thompson from IT Solutions Ltd.
+      ],
+    },
+    {
+      name: 'Sarah Williams',
+      jobTitle: 'Digital Marketing Specialist Recruiter',
+      agency: 'Marketing Experts Inc.',
+      contactEmail: 'sarah.williams@marketingexperts.com',
+      jobPostings: [
+        {
+          title: 'Digital Marketing Specialist',
+          deployedLink: '/',
+          description: 'A Digital Marketing Specialist is responsible for planning, executing, and monitoring digital marketing campaigns. They should have knowledge of SEO, social media marketing, content creation, and digital advertising platforms.',
+        },
+        // Other job postings by Sarah Williams from Marketing Experts Inc.
+      ],
+    },
+    {
+      name: 'Alex Rodriguez',
+      jobTitle: 'Product Manager Recruiter',
+      agency: 'Tech Innovators',
+      contactEmail: 'alex.rodriguez@techinnovators.com',
+      jobPostings: [
+        {
+          title: 'Product Manager',
+          deployedLink: '/',
+          description: 'A Product Manager is responsible for developing and managing the strategy, roadmap, and feature definition for a product or product line. They should have strong analytical and communication skills, and be able to collaborate with cross-functional teams.',
+        },
+        // Other job postings by Alex Rodriguez from Tech Innovators
+      ],
+    },
+    // Add more recruiters and their job postings as needed
+  ];
+  
+  export default recruiterData;
+  
