e450de5484feede4f15a9b0539f662b82b4159d4
Anthony Vaquer <anthonymark126@gmail.com>
2023-06-08 13:06:18 -0700

    added regex to search

--- client/src/pages/SearchResults.js
+++ client/src/pages/SearchResults.js
@@ -7,40 +7,53 @@ export default function SearchResults() {
 
   const databaseSample = [
     {
-      title: "jr-developer",
+      title: "junior developer",
       company: "Dude Inc",
       description: "assist sr dev",
       salary: 50000,
       applicantNumber: 10,
     },
     {
-        title: "jr-developer",
-        company: "Kramerica Industries",
-        description: "build web apps",
-        salary: 60000,
-        applicantNumber: 5,
-      },
+      title: "jr developer",
+      company: "Kramerica Industries",
+      description: "build web apps",
+      salary: 60000,
+      applicantNumber: 5,
+    },
     {
       title: "backend-developer",
       company: "dudette inc",
     },
   ];
 
-  function filterSearch() {
+  function filterSearch() { 
+    // TODO - add Regex for jr/sr/dev/developer/any other common searchs that come to mind
     const searchWords = word.split(" ").filter(Boolean);
-    const data = databaseSample.filter((item) => item.title === word);
-    
-    // console.log(data);
+    const regexes = searchWords.map((word) => {
+      const regexPattern = word.replace(
+        /\b(jr|sr)\b/gi,
+        "(jr|sr|junior|senior)"
+      );
+      return new RegExp(regexPattern, "i");
+    });
+    const data = databaseSample.filter((item) =>
+      regexes.some((regex) => regex.test(item.title))
+    );
 
     return data.length > 0 ? (
-      data.map((content) => (
-        <Card style={{ width: "18rem", marginBottom: "10px", marginTop: "10px", }}>
+      data.map((content, index) => (
+        <Card
+          style={{ width: "18rem", marginBottom: "10px", marginTop: "10px" }}
+          key={index}
+        >
           <Card.Body>
             <Card.Title>{content.title}</Card.Title>
             <Card.Text>Company: {content.company}</Card.Text>
             <Card.Text>Description: {content.description}</Card.Text>
             <Card.Text>Salary: {content.salary}</Card.Text>
-            <Card.Text>Number of Applicants: {content.applicantNumber}</Card.Text>
+            <Card.Text>
+              Number of Applicants: {content.applicantNumber}
+            </Card.Text>
             <Button variant="primary">View</Button>
           </Card.Body>
         </Card>
