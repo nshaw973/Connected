4194f93c7cb2f9faa829c937b79c8c011db65aa8
nshaw973 <nshaw973@gmail.com>
2023-06-11 16:53:12 -0700

    deploy 2

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -197,12 +197,12 @@ const Navbar = () => {
                 </>
               ) : (
                 <>
-                  <a
-                    href="/login"
+                  <Link
+                    to="/login"
                     className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                   >
                     Login
-                  </a>
+                  </Link>
                 </>
               )}
             </div>
