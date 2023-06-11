20ffb7bb9f390cc5e30647ca86a92ea7c5e852a7
Shawn Lapuz <lapuzshawn@gmail.com>
2023-06-10 20:05:18 -0700

    added job post form to dashboard

--- client/src/components/NewPost.js
+++ client/src/components/NewPost.js
@@ -1,31 +1,49 @@
-import React from 'react';
-import { Button, Col } from 'react-bootstrap';
+import React, { useState } from 'react';
 
-function newPost() {
-  
-    return (
-      <>
-      <Button className="stat-value">Create a new job post</Button>
-      <br></br>
-        <div className="card" id="newPostForm">
+function NewPost() {
+  const [formVisible, setFormVisible] = useState(false);
+
+  const handleNewPostClick = () => {
+    setFormVisible((prevVisible) => !prevVisible);
+  };
+
+  return (
+    <>
+      
+    <div className="stats bg-primary text-primary-content">
+        <div className="stat">
+          {/* <Button className="stat-value">Create a new job post</Button> */}
+          <button className="stat-value" onClick={handleNewPostClick}>
+               Create a new job post
+          </button>
+        </div>
+      </div>
+
+      <br />
+      {formVisible && (
+        <div className="card" id="newPostForm" style={{ width: '700px' }}>
           <div className="card-body">
             <form>
               <div className="form-group">
-                <label htmlFor="titleInput">Title</label>
+                <label htmlFor="titleInput">Job Title</label>
                 <input type="text" id="postTitle" className="form-control" placeholder="Enter title" />
               </div>
-  
+              <br></br>
               <div className="form-group">
-                <label htmlFor="textInput">Text</label>
-                <textarea id="postContent" className="form-control" rows="3" placeholder="Enter text"></textarea>
+                <label htmlFor="textInput">Job Description</label>
+                <textarea id="postContent" className="form-control" rows="10" placeholder="Enter text"></textarea>
               </div>
-              <button type="submit" className="btn btn-primary" id="postSubmit">Submit</button>
+              <br></br>
+              <button type="submit" className="btn btn-primary" id="postSubmit">
+                Submit
+              </button>
             </form>
           </div>
         </div>
-      </>
-    );
-  }
-  
-  export default newPost;
-  
+      )}
+      <br></br>
+    </>
+  );
+}
+
+export default NewPost;
